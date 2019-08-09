35f6077695b2f6cef48d58293011abe581677190
mgillespie19 <max.gillespie10@gmail.com>
2019-08-09 16:42:25 -0400

    adding origin, adding vstack + hstack

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Max Gillespie on 8/9/19.
+//  Copyright © 2019 Max Gillespie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
